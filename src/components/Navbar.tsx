c298d5fc05655cfd8c4c80a3d42237c9e8766c3d
Tina Flower <tmflower@gmail.com>
2023-04-12 16:07:48 -1000

    build store page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
-import { Navbar as NavbarBs, Nav, Container } from 'react-bootstrap'
+import { Navbar as NavbarBs, Nav, Container, Button } from 'react-bootstrap'
 import cart from '../assets/shopping-cart.png'
 import { NavLink } from 'react-router-dom'
 
 export function Navbar() {
 	return (
-		<NavbarBs className="bg-white shadow-sm mb-3">
+		<NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
 			<Container>
-				<Nav>
+				<Nav className="me-auto">
 					<Nav.Link to="/" as={NavLink}><img src={cart} width="50"/></Nav.Link>
 				</Nav>
 				<Nav>
@@ -15,6 +15,19 @@ export function Navbar() {
 				<Nav>
 					<Nav.Link to="/about" as={NavLink}>About</Nav.Link>
 				</Nav>
+				<Button
+					style={{ paddingLeft: 10, paddingRight: 10, marginLeft: 10, position: "relative" }}
+					variant="outline-primary">Checkout
+					<div
+						style={{
+							color: "white",
+							width: "1.5rem",
+							height: "1.5rem",
+							position: "absolute",
+							bottom: 0,
+							right: 0,
+							transform: "translate(40%, 40%)",}}className="rounded-circle bg-danger d-flex justify-content-center">3
+					</div></Button>
 			</Container>
 
 		</NavbarBs>
